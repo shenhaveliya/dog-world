00d24053f38000aa5b586b4eef7d317b5c73dd21
shenhaveliya <shenhaveliya@users.noreply.github.com>
2026-05-25 18:26:32 +0000

    deploy: 2fdbf7996f5cc4d9da0f584b2f81046d930e9c07

---
+++ previews/pr-15/service-worker.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const CACHE_NAME = "dog-world-v42";
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css?v=20260520-ux-pack",
+  "./script.js?v=20260520-ux-pack",
+  "./breeds.js",
+  "./wiki-images.js",
+  "./i18n.js?v=20260520-ux-pack",
+  "./breed-food.js",
+  "./manifest.json",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  if (url.hostname.includes("dog.ceo") || url.hostname.includes("dogceo")) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+});
